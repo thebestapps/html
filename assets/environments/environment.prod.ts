9f2fdcb8a93a34cfe8978fef6f24b870a3608a38
Verma <shubamverma@gmail.com>
2023-09-16 16:25:05 +0530

    Fix

---
+++ assets/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+
+  apiUrl: 'https://backend.kakoo-software.com/',
+  apiUrl_v1: 'https://backend.kakoo-software.com/kakoo-back-end/api/v1/',
+
+  // Staging 2
+
+  // apiUrl: 'https://web.kakoo-software.com/kakoo-back-end/',
+  // apiUrl_v1: 'https://web.kakoo-software.com/kakoo-back-end/api/v1/',
+
+  // Local 2
+
+  //apiUrl: 'http://localhost:9090/',
+  //socketUrl: 'ws://localhost:9090/'
+};
